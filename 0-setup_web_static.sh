ddca5493aa54dd2893ffd9c0db91f3e9ca1f962e
Etsybt <hmidouchnouhaila2@gmail.com>
2024-01-11 16:13:35 +0000

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
-#sets up my web servers for the deployment of web_static
-
-apt-get update
-apt-get -y install nginx
+#nstall Nginx if not already installed
+sudo apt-get -y update
+sudo apt-get -y install nginx
+mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>' > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu:ubuntu /data/
-sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-service nginx restart
+echo "simple content, to test your Nginx configuration" >> /data/web_static/releases/test/index.html
+ln -fs /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i "/listen 80 default_server;/a location /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
+sudo ufw allow "Nginx HTTP"
+sudo service nginx restart
+exit 0
