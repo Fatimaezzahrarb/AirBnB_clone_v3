e3bafd2e22fe0f80dfdf093086af65111e48f7bc
Etsybt <hmidouchnouhaila2@gmail.com>
2024-01-11 17:00:42 +0000

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,6 +6,7 @@ from fabric.api import put, env, run
 env.hosts = ["54.160.113.165", "34.227.93.131"]
 env.user = "ubuntu"
 
+
 def do_deploy(archive_path):
     """deploy package"""
     if not archive_path or not os.path.isfile(archive_path):
@@ -17,10 +18,12 @@ def do_deploy(archive_path):
 
     put(local_path=archive_path, remote_path="/tmp/")
     run("mkdir -p /data/web_static/releases/{}".format(release_name))
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(archive_name, release_name))
+    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+        archive_name, release_name))
     run("rm /tmp/{}".format(archive_name))
     run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(release_name))
+    run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(
+        release_name))
     run("mv /data/web_static/current/web_static/* /data/web_static/current/")
     run("rm -rf /data/web_static/current/web_static")
 
