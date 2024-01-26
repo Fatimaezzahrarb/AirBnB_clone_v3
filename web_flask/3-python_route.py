25deb1c8df43f682ce6c7d28deb924c3fd2e7189
Etsybt <hmidouchnouhaila2@gmail.com>
2024-01-26 14:06:49 +0000

    update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,7 +27,7 @@ def cisfun(text):
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text):
+def python_route(text="is cool"):
     """display “Python ”, followed by the value of the text"""
     return 'Python ' + text.replace('_', ' ')
 
