d2fe636d4041b0f3f8d6e9460303e68bbd4a78a0
Etsybt <hmidouchnouhaila2@gmail.com>
2024-01-29 15:01:42 +0000

    update

--- models/city.py
+++ models/city.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-'''
-This module defines the City class.
-'''
+"""class City."""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
-# Check the storage type
+
 storage_type = getenv("HBNB_TYPE_STORAGE")
+
 
 class City(BaseModel, Base):
     '''
-    This class represents a City and inherits from BaseModel.
+        Define the class City that inherits from BaseModel.
     '''
     __tablename__ = 'cities'
-
-    # Define columns based on storage type
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", backref="cities", cascade="all, delete-orphan")
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete-orphan")
     else:
         name = ""
         state_id = ""
