1db505bb0e3d96f42605b64550b9f804ed57535d
Etsybt <hmidouchnouhaila2@gmail.com>
2024-01-29 00:17:17 +0000

    update

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-from models.city import City
 
 app = Flask(__name__)
 
