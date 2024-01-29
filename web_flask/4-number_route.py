09f53216c019dc41361196b2dcf8d8985e67127e
Fatimaezzahrarb <north.winds04@gmail.com>
2024-01-28 17:31:22 -0800

    flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Fourth view function
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """return Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """show “C ” followed by value of text variable"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """show “Python ”, followed by the value of text variable"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    """show “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')i
